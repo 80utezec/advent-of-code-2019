f3b6b3e3c8030d726425bfd1e6e8c2c883677eb1
Zechariah Utecht <zechariah.utecht@menasha.com>
2019-12-13 10:08:32 -0600

    Day03 without testing

--- src/main/java/com/utecht/Main.java
+++ src/main/java/com/utecht/Main.java
@@ -1,7 +1,5 @@
 package com.utecht;
 
-import java.io.File;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,15 +8,22 @@ public class Main{
 	public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 	
 	public static void main(String[] args){
-		final File day01File = new File("src/test/resources/input/day01.txt");
-		final Day01 day01 = new Day01(day01File);
-		LOGGER.info("Day 01, part01: {}", day01.part01());
-		LOGGER.info("Day 01, part02: {}", day01.part02());
+//		final File day01File = new File("src/test/resources/input/day01.txt");
+//		final Day01 day01 = new Day01(day01File);
+//		LOGGER.info("Day 01, part01: {}", day01.part01());
+//		LOGGER.info("Day 01, part02: {}", day01.part02());
+//		
+//		final File day02File = new File("src/test/resources/input/day02.txt");
+//		final Day02 day02 = new Day02(day02File);
+//		LOGGER.info("Day 02, part01: {}", day02.part01(12, 2));		
+//		LOGGER.info("Day 02, part02: {}", day02.part02(19690720));
+//		
+//		final File day03File = new File("src/test/resources/input/day03.txt");
+//		final Day03 day03 = new Day03(day03File);
+//		LOGGER.info("Day 03, part01: {}", day03.part01());
+//		LOGGER.info("Day 03, part02: {}", day03.part02());
 		
-		final File day02File = new File("src/test/resources/input/day02.txt");
-		final Day02 day02 = new Day02(day02File);
-		LOGGER.info("Day 02, part01: {}", day02.part01(12, 2));		
-		LOGGER.info("Day 02, part02: {}", day02.part02(19690720));
+		
 	}
 	
 }
